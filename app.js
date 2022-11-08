39b204d02e8f4cf7873bd37f80146c0357f625e2
Rémy Bernardin <remy.bernardin.sen@gmail.com>
2022-11-08 14:17:06 +0100

    Quetes N°3

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,9 +15,15 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const usersHandlers = require("./usersHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", usersHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
